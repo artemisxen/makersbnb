363bc2d9b09aa42e46b0350b1a84e8a4e775e1fe
annalaise <annalaise.gibbons@gmail.com>
2017-08-09 15:39:29 +0100

    adding layout file and refactoring partials

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,8 +13,9 @@ var signup = require('./routes/signup');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
